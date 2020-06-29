066131926baba7fa8a0d8f7039edef556132c6b4
Ian Johnson <johnson882@gmail.com>
2020-06-28 23:59:14 -0700

    fixed vote list

--- products/models.py
+++ products/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
@@ -8,11 +15,10 @@ class Product(models.Model):
     url = models.TextField()
     image = models.ImageField(upload_to='images/')
     icon = models.ImageField(upload_to='images/')
-    votes_total = models.IntegerField(default=1)
+    #votes_total = models.IntegerField(default=1)
     hunter = models.ForeignKey(User, on_delete=models.CASCADE)
-    votes_total = models.ManyToManyField(User, related_name="votes", blank=True)
+    votes_total = models.ManyToManyField(Vote, related_name="votes", blank=True)
 
-    
     def __str__(self):
         return self.title
 
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, Vote
 from django.utils import timezone
 
 def home(request):
@@ -22,7 +22,16 @@ def create(request):
             product.image = request.FILES['image']
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
+            vote = Vote()
+
+            vote.user = request.user
+            vote.save()
             product.save()
+            product.votes_total.add(vote)
+            product.save()
+
+            print("count for product", product.votes_total.count())
+
             return redirect('/products/' + str(product.id))
         else:
             return render(request, 'products/create.html',{'error':'All fields are required.'})
@@ -31,6 +40,7 @@ def create(request):
 
 def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
+    print("here is the total vote count:", product.votes_total.all().count())
     return render(request, 'products/detail.html',{'product':product})
 
 @login_required(login_url="/accounts/signup")
